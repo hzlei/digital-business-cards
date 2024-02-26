26131f2aece61ac26c59a821021256b0c134a78e
Brain Zheng <cccnmsnn@gmail.com>
2024-02-25 20:28:39 -0500

    Extremely basic screens for sharing and recieving
    I hate google and their depreceation warnings

--- app/src/main/java/cs446/dbc/MainActivity.kt
+++ app/src/main/java/cs446/dbc/MainActivity.kt
@@ -1,46 +1,33 @@
 package cs446.dbc
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import cs446.dbc.ui.theme.DBCTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
-class MainActivity : ComponentActivity() {
+interface NavigationHost {
+    fun navigateTo(fragment: Fragment, addToBackstack: Boolean)
+}
+class MainActivity : AppCompatActivity(), NavigationHost {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DBCTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.dbc_main_activity)
+        
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container, MainMenuFragment())
+            .commit()
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DBCTheme {
-        Greeting("Android")
+        if (addToBackstack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
     }
 }
