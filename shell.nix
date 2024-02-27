7286224c12d8ff9aa539032ac8db7c99753d759e
Mrugank Upadhyay <mrugank2@gmail.com>
2024-02-27 18:50:32 -0500

    reverted jdk version to 21

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ jdk17 kotlin ];
+    nativeBuildInputs = with pkgs.buildPackages; [ jdk21 kotlin gradle ];
 }
