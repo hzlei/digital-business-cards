ea02e47994aed071d93ce9e3f85ae99c492a0134
Mrugank Upadhyay <mrugank2@gmail.com>
2024-02-28 14:54:09 -0500

    new changes to test bluetooth permissions request

--- app/src/main/java/cs446/dbc/bluetooth/Bluetooth.kt
+++ app/src/main/java/cs446/dbc/bluetooth/Bluetooth.kt
@@ -25,36 +25,69 @@ class Bluetooth(context: Context): AppCompatActivity() {
         Toast.makeText(context, "Bluetooth exists", Toast.LENGTH_SHORT).show()
         if (bluetoothAdapter?.isEnabled == false) {
             Toast.makeText(context, "Bluetooth is disabled", Toast.LENGTH_SHORT).show()
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            ContextCompat.startActivity(context, enableBtIntent, ActivityOptionsCompat.makeBasic().toBundle())
-            // First check if we even have permission to use bluetooth
-            if (ActivityCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.BLUETOOTH_CONNECT
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                val BLUETOOTH_RESULT_CODE = 5
-                ActivityCompat.requestPermissions(
-                    context as Activity,
-                    arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
-                    BLUETOOTH_RESULT_CODE
-                )
-                val permReqLauncher =
-                    registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-                        if (isGranted) {
-                            startActivity(enableBtIntent)
-                        } else {
-
-                        }
+            val requestMultiplePermissionsLauncher =
+                registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+                    isGranted ->
+                    if (isGranted.getOrElse("BLUETOOTH") { false }) {
+                        Toast.makeText(
+                            context,
+                            "Bluetooth Permission Already Granted",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                       Toast.makeText(context, "Sorry, but we don't have the permission necessary for bluetooth", Toast.LENGTH_SHORT).show()
                     }
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+                }
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.BLUETOOTH
+                ) != PackageManager.PERMISSION_GRANTED) {
+                requestMultiplePermissionsLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.BLUETOOTH,
+                        Manifest.permission.BLUETOOTH_ADMIN,
+                        Manifest.permission.BLUETOOTH_SCAN,
+                        Manifest.permission.BLUETOOTH_CONNECT,
+                        Manifest.permission.BLUETOOTH_ADVERTISE))
             }
+//            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+
+//            ContextCompat.startActivity(context, enableBtIntent, ActivityOptionsCompat.makeBasic().toBundle())
+            // First check if we even have permission to use bluetooth
+//            if (ActivityCompat.checkSelfPermission(
+//                    context,
+//                    Manifest.permission.BLUETOOTH_CONNECT
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                val BLUETOOTH_RESULT_CODE = 5
+//                val permissions = mutableSetOf(
+//                    Manifest.permission.BLUETOOTH,
+//                    Manifest.permission.BLUETOOTH_ADMIN,
+//                    Manifest.permission.BLUETOOTH_SCAN,
+//                    Manifest.permission.BLUETOOTH_CONNECT,
+//                    Manifest.permission.BLUETOOTH_ADVERTISE
+//                )
+//                ActivityCompat.requestPermissions(
+//                    context as Activity,
+//                    permissions.toTypedArray(),
+//                    BLUETOOTH_RESULT_CODE
+//                )
+//                val permReqLauncher =
+//                    registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+//                        if (isGranted) {
+//                            startActivity(enableBtIntent)
+//                        } else {
+//
+//                        }
+//                    }
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//            }
         }
         else {
             Toast.makeText(context, "Bluetooth already enabled!", Toast.LENGTH_SHORT).show()
