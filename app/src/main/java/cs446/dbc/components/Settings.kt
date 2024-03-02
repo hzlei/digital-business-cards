1baaa9d8fc33dcc507495ea1c7ef18f15cb3f290
Hank Wu <79679936+hzlei@users.noreply.github.com>
2024-03-02 10:22:51 -0500

    Create Component with Material3
    Create Component for Settings

---
+++ app/src/main/java/cs446/dbc/components/Settings.kt
@@ -0,0 +1,12 @@
+package cs446.dbc.components
+
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+
+@Composable
+fun Settings() {
+    IconButton(onClick = { /*TODO*/ }) {
+
+    }
+}
